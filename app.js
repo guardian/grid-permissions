f672216ca0063e7bb7031f5669f05c87d8363b05
Robert Kenny <robert.kenny@guardian.co.uk>
2016-04-22 13:32:30 +0100

    error handling for logged out state

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 import Rx from 'rx';
 import Ractive from 'ractive';
 
+
+import env from '../config/current-env.js';
+import apiUriByEnv from '../config/auth-api.js';
+
 import {Permissions} from '../lib/permissions.js';
 
 var ractive = new Ractive({
   el: '#container',
   template: '#template',
-  data: { store: {} } 
+  data: { store: {}, error: false } 
 });
 
-const current$ = Permissions.authGroups$ 
+const current$ = Permissions.authGroups$;
 
-current$.subscribe((m) => ractive.set('store', m));
+current$.catch((e) => {
+  console.log("CAUGHT!");
+  ractive.set('error', true)
+});
+
+current$.subscribe(
+    (m) => { 
+      ractive.set('store', m)
+    },
+    (e) => {
+      ractive.set('redirectUrl', window.location.href)
+      ractive.set('apiUrl', apiUriByEnv[env])
+      ractive.set('error', true)
+    }
+);
